d267f853714392ce1740bd212b3eeaaba07506e8
Gokul Pulikkal <g.gokuldas68@gmail.com>
2025-03-11 22:53:34 -0400

    Initial Commit

---
+++ PoopDetector/PoopDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoopDetectorApp.swift
+//  PoopDetector
+//
+//  Created by Gokul P on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PoopDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoopDetectorTests/PoopDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PoopDetectorTests.swift
+//  PoopDetectorTests
+//
+//  Created by Gokul P on 3/11/25.
+//
+
+import Testing
+@testable import PoopDetector
+
+struct PoopDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
